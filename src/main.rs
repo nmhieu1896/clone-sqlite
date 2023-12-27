f5054325c893b22a5ad65f8fadbe127f1d77897c
Hieu Nguyen <nmhieu1896@gmail.com>
2023-12-27 22:08:41 +0700

    Why it's so hard

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,42 @@ fn main() -> Result<()> {
                         .count()
             );
         }
+        ".tables" => {
+            let mut file = File::open(&args[1])?;
+            let mut header = [0; 100];
+            file.read_exact(&mut header)?;
+
+            // let reader = BufReader::new(&file);
+            // reader.lines().for_each(|line| {
+            //     println!("line:{}", line.unwrap_or("_____".to_string()));
+            // match line {
+            //     Ok(line) => {
+            //         println!("line:{}", line);
+            //         // if line.starts_with("CREATE TABLE") {
+            //         //     println!("{}", line.split("(").next().unwrap());
+            //         // }
+            //     }
+            //     Err(e) => println!("Error: {}", e),
+            // }
+            // let line = line.unwrap();
+            // if line.starts_with("CREATE TABLE") {
+            //     println!("{}", line.split("(").next().unwrap());
+            // }
+            // });
+
+            let page_size = u16::from_be_bytes([header[16], header[17]]);
+            let mut body = vec![0; (page_size - 100).into()];
+            file.read_exact(&mut body)?;
+
+            let tables = String::from_utf8_lossy(&body)
+                .split("CREATE TABLE ")
+                .skip(1)
+                .map(|s| s.split("\n(").next().unwrap().to_string())
+                .filter(|s| !s.starts_with("sqlite_"))
+                .collect::<Vec<String>>()
+                .join(" ");
+            println!("{}", tables);
+        }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
 
